536149588cd2796e3f6712f2cdb0b3bc5977c737
Wira Siwananda <siwananda@gmail.com>
2013-06-20 23:53:38 +0800

    master
    the first commit

---
+++ src/main/java/com/moneymatters/data/HelloService.java
@@ -0,0 +1,10 @@
+package com.moneymatters.data;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloService {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
